d7bf56c4a9edb64d2f1e45ab3a0a613e24691e6c
Zack Liscio <zackliscio@gmail.com>
2012-07-05 16:25:20 -0400

    fixed error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   provider :facebook,'122357404569412', '5f7e5f45646bde81f5dc45ceb7af8131'
-  provider :facebook,'rAzkvztbQ2tZW8sgX95qw','eijvbHnAC6MNaVwNwbsm4AxdrDQWecfFHowb53ZEg0'
+  provider :twitter,'rAzkvztbQ2tZW8sgX95qw','eijvbHnAC6MNaVwNwbsm4AxdrDQWecfFHowb53ZEg0'
   # provider :facebook, ENV['122357404569412'], ENV['5f7e5f45646bde81f5dc45ceb7af8131']
 end
